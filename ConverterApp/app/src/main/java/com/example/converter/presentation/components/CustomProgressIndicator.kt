595bd93fc043d3193bb74749d89981b5584803ef
Fernando Salom <fer@rudo.es>
2023-11-24 11:13:40 +0100

    Add loader component

---
+++ ConverterApp/app/src/main/java/com/example/converter/presentation/components/CustomProgressIndicator.kt
@@ -0,0 +1,27 @@
+package com.example.converter.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomProgressIndicator() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.size(32.dp),
+            color = Color.LightGray,
+            strokeWidth = 5.dp
+        )
+    }
+}
